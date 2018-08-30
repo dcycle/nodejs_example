139a3ff829be0d63523796a49d4e536db3baaf9b
alberto56 <albert@mediatribe.net>
2018-08-30 15:39:49 -0400

    Use pseudo-data provider

--- node/test/test.js
+++ node/test/test.js
@@ -1,10 +1,14 @@
 var expect = require('chai').expect;
 var log = require('../my/mylogger');
 
+
 describe('addTwoNumbers()', function () {
+  // This is the equivalent of data providers in PHPUnit.
+  // See https://gist.github.com/burkhardr/54162f667ff756540574082f61350b38.
   const assertions = [
     { first: 5, second: 1 },
     { first: -10000, second: -80 },
+    { first: -9999999999999999, second: -9999999999999999 },
   ];
 
   assertions.forEach(({first, second}) => {
